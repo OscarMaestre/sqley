6c4a380fc3e0613cedf96715b283cc9c59c1d66a
Oscar G.G <profesor.oscar.gomez@gmail.com>
2016-10-21 18:37:50 +0200

    Añadidas letras

--- gestionbd/views.py
+++ gestionbd/views.py
@@ -6,11 +6,11 @@ from .models import *
 
 
 control="""
-<input type="text" value="{0}" size="130" onclick="this.select();document.execCommand('copy');">
+<input type="text" value="{0}" size="135" onclick="this.select();document.execCommand('copy');">
 """
 
 def get_objetivos_generales_html(ciclo_pasado):
-    resultado="<h3>Objetivos generales</h3>"
+    resultado="<h2>Objetivos generales del ciclo</h2>"
     competencias=ObjetivoGeneral.objects.filter(ciclo=ciclo_pasado)
     for c in competencias:
         resultado+=control.format(c.texto)
@@ -19,7 +19,7 @@ def get_objetivos_generales_html(ciclo_pasado):
 
 
 def get_competencias_html(ciclo_pasado):
-    resultado="<h3>Competencias profesionales</h3>"
+    resultado="<h2>Competencias profesionales del ciclo</h2>"
     competencias=Competencia.objects.filter(ciclo=ciclo_pasado)
     for c in competencias:
         resultado+=control.format(c.texto)
@@ -27,7 +27,7 @@ def get_competencias_html(ciclo_pasado):
 
 
 def get_competencias_generales_html(ciclo_pasado):
-    resultado="<h3>Competencias generales</h3>"
+    resultado="<h2>Competencias generales del ciclo</h2>"
     competencias=CompetenciaGeneral.objects.filter(ciclo=ciclo_pasado)
     for c in competencias:
         resultado+=control.format(c.texto)
@@ -37,15 +37,16 @@ def get_competencias_generales_html(ciclo_pasado):
 
 def get_cualificaciones_profesionales_html(ciclo_pasado, completas):
     if completas:
-        resultado="<h3>Cualificaciones profesionales completas</h3>"
+        resultado="<h2>Cualificaciones profesionales completas del ciclo</h2>"
     else:
-        resultado="<h3>Cualificaciones profesionales incompletas</h3>"
+        resultado="<h2>Cualificaciones profesionales incompletas del ciclo</h2>"
     ciclo_cuali=CicloTieneCualificacion.objects.filter(
         ciclo=ciclo_pasado, es_completa=completas
     )
     
     for c in ciclo_cuali:
-        resultado+=control.format(c.cualificacion_profesional.texto)
+        resultado+=control.format(c.cualificacion_profesional.identificador +
+                                  ") " + c.cualificacion_profesional.texto)
     return resultado
 
 
@@ -57,7 +58,7 @@ def get_resultados_aprendizaje_html(modulo):
     return resultado
 
 def get_elementos_html(ciclo_pasado):
-    resultado="<h1>{0}</h1>".format(ciclo_pasado.nombre)
+    resultado="<h2>{0}</h2>".format(ciclo_pasado.nombre)
     resultado+=get_objetivos_generales_html(ciclo_pasado)
     resultado+=get_competencias_generales_html(ciclo_pasado)
     resultado+=get_competencias_html(ciclo_pasado)
@@ -67,7 +68,11 @@ def get_elementos_html(ciclo_pasado):
     return resultado
 
 def get_datos_modulo(modulo):
-    resultado="<h3>"+modulo.nombre+"</h3>"
+    c=modulo.curso.ciclo
+    resultado=""
+    
+    resultado+="<h1>"+modulo.nombre+"</h1>"
+    resultado+=get_elementos_html(c)
     resultado+=get_contenidos_modulo(modulo)
     resultado+=get_resultados_aprendizaje_modulo(modulo)
     return resultado
@@ -84,13 +89,15 @@ def get_resultados_aprendizaje_modulo(modulo_pasado):
     resultado="<h3>Resultados de aprendizaje para {0}</h3>".format(modulo_pasado.nombre)
     resultados_aprendizaje=ResultadoDeAprendizaje.objects.filter(modulo=modulo_pasado)
     for ra in resultados_aprendizaje:
+        resultado+="<hr/><h4>Resultado</h4>"
         resultado+=control.format(str(ra.numero) + ") " + ra.texto)
         crit_evaluacion=CriterioDeEvaluacion.objects.filter(
             resultado_de_aprendizaje=ra
         )
-        resultado+="<h4>Criterios para {0}</h4>".format(ra.texto)
+        resultado+="<h4>Criterios para {0}) {1}</h4>".format(ra.numero, ra.texto)
         for ce in crit_evaluacion:
             resultado+=control.format(ce.letra+") "+ce.texto)
+        
         
     return resultado
 
@@ -98,7 +105,7 @@ def cortar_elementos ( peticion ):
     ciclo=Ciclo.objects.all().order_by("nombre");
     resultado=""
     for c in ciclo:
-        resultado+=get_elementos_html(c)
+        
         cursos=Curso.objects.filter(ciclo=c).order_by("num_curso")
         for cu in cursos:
             modulos=Modulo.objects.filter(curso=cu).order_by("nombre")
