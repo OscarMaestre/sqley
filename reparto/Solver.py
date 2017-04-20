6818fb353ba9563e68421f63011a590e4f3c268a
Oscar GG <profesor.oscar.gomez@gmail.com>
2017-04-20 17:53:16 +0200

    Iniciando backtracking

--- reparto/Solver.py
+++ reparto/Solver.py
@@ -4,6 +4,7 @@
 from Configurador import Configurador
 configurador=Configurador("..")
 configurador.activar_configuracion("ciclos.settings")
+import copy
 
 from django.db.models import Q
 from gestionbd.models import Profesor, Modulo, EspecialidadProfesor
@@ -11,8 +12,9 @@ import itertools
 
 class ProfesorEnReparto(object):
     def __init__(self, profesor):
-        self.horas_asignadas=0
-        self.horas_maximas=profesor.horas_maximas
+        self.horas_asignadas    =   0
+        self.horas_minimas      =   profesor.horas_minimas
+        self.nombre             =   profesor.nombre
         
         self.modulos_asignados=[]
         
@@ -35,10 +37,28 @@ class GestorProfesores(object):
     def __init__(self):
         self.lista_profesores=[]
         self.siguiente_profesor=0
-    def anadir_profesor(self, profesor):
-        self.lista_profesores.append ( profesor )
         
-    def todos_llenan_horarios(self):
+        
+    def anadir_profesores(self, lista_modelos_profesor):
+        for modelo in lista_modelos_profesor:
+            prof=ProfesorEnReparto(modelo)
+            self.lista_profesores.append ( prof )
+        self.cantidad_profesores=len(self.lista_profesores)
+        
+    
+    def siguiente_profesor(self):
+        profesor_a_devolver=self.lista_profesores[self.siguiente_profesor]
+        self.siguiente_profesor=(self.siguiente_profesor+1) % self.cantidad_profesores
+        
+    def asignar_modulo_a_profesor(self, profesor, modulo):
+        nombre_a_buscar=profesor.nombre
+        for profesor in self.lista_profesores:
+            if profesor.nombre==nombre_a_buscar:
+                profesor.asignar_modulo(modulo)
+                return
+        print ("Error, no se encontro un profesor llamado:"+nombre_a_buscar)
+        
+    def todos_llenan_horario(self):
         for p in self.lista_profesores:
             if not p.llena_horario:
                 return False
@@ -53,19 +73,22 @@ class Solver(object):
         cantidad_modulos=len(self.modulos)
         combinaciones=itertools.combinations(self.modulos, cantidad_modulos-1)
         combinaciones=itertools.permutations(self.modulos)
-        print(dir(combinaciones))
         
-        i=0
-        for indice, combinacion in enumerate(combinaciones):
-            i=i+1
-            if i%10000000==0:
-                print (i)
-            continue
-            print("############     " + str(indice) + "   #####################")
-            print (combinacion)
-            print("############     " + str(indice) + "   #####################")
-        print ("Total:"+str(i))
         
+        
+    def backtracking(self, lista_profesores, lista_modulos, num_sol=0):
+        gestor_nuevo=copy.deepcopy(lista_profesores)
+        print(lista_profesores.lista_profesores)
+        while not gestor_nuevo.todos_llenan_horario():
+            profesor=gestor_nuevo.siguiente_profesor()
+            
+            for modulo in lista_modulos:
+                profesor.asignar_modulo(modulo)
+                lista_modulos.remove(modulo)
+                print("Pasando a la etapa:"+str(num_sol))
+                self.backtracking(lista_profesores, lista_modulos, num_sol+1)
+                print("Cerrando la etapa:"+str(num_sol))
+        print ("Fin")
     def get_modulos(self):
         filtro_modulos_ps=Q(especialidad="PS")
         filtro_modulos_todos=Q(especialidad="TODOS")
@@ -81,4 +104,8 @@ class Solver(object):
         
 if __name__ == '__main__':
     solver=Solver()
+    profesores=solver.profesores
+    gestor=GestorProfesores()
+    gestor.anadir_profesores(profesores)
+    solver.backtracking(gestor, solver.modulos)
     
