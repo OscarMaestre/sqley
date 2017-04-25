7c615acb5069a76fb11512cbe8ff9987a601d456
Oscar GG <profesor.oscar.gomez@gmail.com>
2017-04-25 13:42:58 +0200

    Trabajando con preferencias

--- reparto/urls.py
+++ reparto/urls.py
@@ -2,8 +2,10 @@
 from django.conf.urls import url
 
 from . import views
-
 app_name = 'reparto'
 urlpatterns = [
     url(r'^index$', views.index, name="index"),
+    url(r'^asignar$', views.asignar_preferencias, name="asignar_preferencias"),
+    url(r'^almacenar_preferencias/(?P<id_profesor>[0-9]+)/$',
+        views.almacenar_preferencias, name="almacenar_preferencias"),
 ]
--- reparto/views.py
+++ reparto/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import RepartoForm, Reparto
+from .models import RepartoForm, Reparto, Asignacion, PreferenciaProfesor, ModuloEnReparto
+from gestionbd.models import Profesor
 from django.http import HttpResponseRedirect
 from django.db import connection, transaction
+from django.forms import formset_factory, ModelForm
 from django.core.urlresolvers import reverse
 
 EXTRACCION_MODULOS="""
@@ -21,9 +23,47 @@ SELECT mod.id, mod.nombre, mod.horas_semanales,
 """
 # Create your views here.
 
+class AsignacionForm(ModelForm):
+    class Meta:
+        model = PreferenciaProfesor
+        fields=["modulo", "prioridad"]
 
 def index(peticion):
     return render(None, "reparto/index.html")
 
 def repartir(peticion, num_reparto, codigo_profesor, codigo_asignatura):
     return None
+
+
+
+def asignar_preferencias(peticion):
+    profesores=Profesor.objects.all().order_by("num_posicion")
+    
+    contexto=dict()
+    contexto["profesores"]=profesores
+    return render(peticion, "reparto/asignar_preferencias.html", contexto)
+
+def almacenar_preferencias(peticion, id_profesor=None):
+    if peticion.method=='POST':
+        print(peticion)
+        
+        asignaciones_formset=formset_factory(AsignacionForm)
+        formularios=asignaciones_formset(peticion.POST)
+        if formularios.is_valid():
+            print("Todo perfecto")
+            return render(None, "reparto/index.html")
+        else:
+            print("Error de validacion")
+            return render(None, "reparto/index.html")
+    modulos_en_reparto=ModuloEnReparto.objects.all().order_by("modulo_asociado__nombre")
+    profesor=Profesor.objects.get(id=id_profesor)
+    #cantidad_modulos_a_repartir=len(modulos_en_reparto)
+    cantidad_modulos_a_repartir=14
+    contexto=dict()
+    
+    asignaciones_formset=formset_factory(AsignacionForm,
+                extra=cantidad_modulos_a_repartir)   
+    contexto["nombre_profesor"]=Profesor.objects.get(id=id_profesor)
+    contexto["formularios"]=asignaciones_formset
+    contexto["id_profesor"]=id_profesor
+    return render(peticion, "reparto/almacenar_preferencias.html", contexto)
